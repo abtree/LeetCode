2c151f2740e93c125da627662808f88c99e5274d
abtree <abtree@qq.com>
2020-12-18 18:09:14 +0800

    update

---
+++ Permutation2.h
@@ -0,0 +1,106 @@
+﻿#pragma once
+
+//47. 全排列 II
+class Permutation2 {
+	/*
+	给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+
+	输入：nums = [1,1,2]
+	输出：
+	[[1,1,2],
+	[1,2,1],
+	[2,1,1]]
+
+	输入：nums = [1,2,3]
+	输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+	*/
+public:
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
+		vector<vector<int>> mRet;
+		sort(nums.begin(), nums.end());
+		track(mRet, nums, 0, nums.size());
+		return mRet;
+	}
+private:
+	void track(vector<vector<int>>& ret, vector<int>& nums, int first, int len) {
+		//回溯法
+		if (first >= len) {
+			ret.emplace_back(nums);
+			return;
+		}
+
+		unordered_set<int> used;
+		for (int i = first; i < len; ++i) {
+			if (i != first && nums[i] == nums[i - 1]) {
+				if (nums[i] == nums[i - 1])
+					continue;	//和上一个相等 ，first和i交换与和i-1交换一样
+				if (nums[i] == nums[first])
+					continue;	//相等 交换了等于没交换
+			}
+
+			//处理这种情况(5和7相等，4,5 5,7 == 4,7)
+			/*
+				[1,2,0,0,-1,1,-1],
+				[1,2,0,-1,-1,1,0],  //4,5 5,7
+				[1,2,0,-1,-1,1,0],  //4,7
+			*/
+			if (used.count(nums[i]) > 0)
+				continue;
+			used.insert(nums[i]);
+
+			swap(nums[i], nums[first]);			//执行交换
+			track(ret, nums, first + 1, len);
+			swap(nums[i], nums[first]);			//再交换回来
+		}
+	}
+public:
+	static void Test() {
+		Permutation2 cls;
+		vector<vector<int>> ret = cls.permuteUnique(vector<int>{1, 2, 3});
+		cout << "[" << endl;
+		for (auto& it : ret) {
+			cout << "[";
+			for (int i : it) {
+				cout << i << ",";
+			}
+			cout << "]" << endl;
+		}
+		cout << "]" << endl;
+
+		ret.clear();
+		ret = cls.permuteUnique(vector<int>{1, 1, 2});
+		cout << "[" << endl;
+		for (auto& it : ret) {
+			cout << "[";
+			for (int i : it) {
+				cout << i << ",";
+			}
+			cout << "]" << endl;
+		}
+		cout << "]" << endl;
+
+		ret.clear();
+		ret = cls.permuteUnique(vector<int>{2, 2, 1, 1});
+		cout << "[" << endl;
+		for (auto& it : ret) {
+			cout << "[";
+			for (int i : it) {
+				cout << i << ",";
+			}
+			cout << "]" << endl;
+		}
+		cout << "]" << endl;
+
+		ret.clear();
+		ret = cls.permuteUnique(vector<int>{-1, 2, 0, -1, 1, 0, 1});
+		cout << "[" << endl;
+		for (auto& it : ret) {
+			cout << "[";
+			for (int i : it) {
+				cout << i << ",";
+			}
+			cout << "]" << endl;
+		}
+		cout << "]" << endl;
+	}
+};
---
+++ TrapRainWater.h
@@ -0,0 +1,83 @@
+﻿#pragma once
+
+//42. 接雨水
+class CTrapRainWater {
+	/*
+	给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+	
+	输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+	输出：6
+	解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+
+	输入：height = [4,2,0,3,2,5]
+	输出：9
+	*/
+public:
+	int trap(vector<int>& height) {
+		//双指针法
+		int p = 0;
+		int q = height.size() - 1;
+		int ret = 0;
+		while (p < q) {
+			if (height[p] <= height[q]) {
+				//移动左指针
+				int cur = height[p];
+				++p;
+				while (p < q) {
+					if (height[p] <= cur) {
+						ret += cur - height[p];
+						++p;
+					}
+					else {
+						break;
+					}
+				}
+			}
+			else {
+				//移动右指针
+				int cur = height[q];
+				--q;
+				while (p < q)
+				{
+					if (height[q] <= cur) {
+						ret += cur - height[q];
+						--q;
+					}
+					else {
+						break;
+					}
+				}
+			}
+		}
+		return ret;
+	}
+
+	int trap2(vector<int>& height) {
+		//动态规划法
+		if (height.empty())
+			return 0;
+		int ans = 0;
+		int size = height.size();
+		vector<int> left(size), right(size);
+		left[0] = height[0];
+		for (int i = 1; i < size; ++i) {
+			left[i] = max(left[i - 1], height[i]);
+		}
+		right[size - 1] = height[size - 1];
+		for (int i = size - 2; i >= 0; --i) {
+			right[i] = max(right[i + 1], height[i]);
+		}
+		for (int i = 0; i < size; ++i) {
+			ans += min(left[i], right[i]) - height[i];
+		}
+		return ans;
+	}
+public:
+	static void Test() {
+		CTrapRainWater cls;
+		int ret = cls.trap2(vector<int>{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1});
+		cout << ret << " --> 6" << endl;
+		ret = cls.trap2(vector<int>{4, 2, 0, 3, 2, 5});
+		cout << ret << " --> 9" << endl;
+	}
+};
